62e5558d37d0d2549d5934264ad550df8a80c260
Lara Abreu <laraabreu1129@gmail.com>
2025-05-28 00:55:19 +0000

    resolução da Tarefa - Tipos

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
-// Escreva seu código aqui!
+import { stripTypeScriptTypes } from "module";
+
+let numeroDeAlunos: number = 22;
+console.log(numeroDeAlunos)
+
+let nomeDoProf: string = "Lana Mesquita";
+console.log(nomeDoProf)
+
+let aulaEmAndamento: boolean = false;
+console.log(aulaEmAndamento)
+
+let nomeDoAluno: string = "Lara Maria";
+console.log(nomeDoAluno)
+
+type Notebook = {
+
+    marca: string;
+    valor: number;
+}
+
+let notebook: Notebook = {
+    
+    marca: "Samsung" , valor: 4000
+}
+
+console.log(`o notebook da marca ${notebook.marca} está custando ${notebook.valor} atualmente.`)
